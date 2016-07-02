53597e433a456cb5a4d655af637e23ae6816304c
Martina Kostovska Inside <martina.kostovska@one-inside.com>
2016-07-02 23:23:47 +0200

    basic project setup

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by martina on 7/2/16.
+ */
+
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App</h1>'
+})
+export class AppComponent {
+}
+
---
+++ app/main.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by martina on 7/2/16.
+ */
+
+import {bootstrap}    from '@angular/platform-browser-dynamic';
+
+import {AppComponent} from './app.component';
+
+bootstrap(AppComponent);
