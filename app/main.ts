51f2abf30695a657778a0823f1b9348554faee49
Martina Kostovska Inside <martina.kostovska@one-inside.com>
2016-07-03 22:30:08 +0200

    bootstrap app (providing form functionality)

--- app/main.ts
+++ app/main.ts
@@ -5,5 +5,10 @@
 import {bootstrap}    from '@angular/platform-browser-dynamic';
 
 import {AppComponent} from './app.component';
+import {disableDeprecatedForms, provideForms} from "@angular/forms";
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+    disableDeprecatedForms(),
+    provideForms()
+])
+    .catch((err:any) => console.error(err));
