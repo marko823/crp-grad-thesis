520d3779a6da3d3e4b0bf4db3dfb2c694cd94f60
Martina Kostovska Inside <martina.kostovska1@gmail.com>
2016-07-04 15:43:07 +0200

    initial dashboard

---
+++ app/dashboard.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+/**
+ * Created by martina on 7/4/16.
+ */
+
+@Component({
+    selector: 'dashboard',
+    template: `<p>Dashboard</p>`
+})
+export class DashboardComponent {
+
+}
